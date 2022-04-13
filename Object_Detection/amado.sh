06f5dc6f1ef2753cc425c4d524bb2afe86473458
Juan Alvarez <65520250+JuanFAlvarez@users.noreply.github.com>
2022-04-13 11:22:52 -0500

    Delete Object_Detection directory

--- Object_Detection/amado.sh
+++
@@ -1,3 +0,0 @@
-cd tflite1/
-source tflite1-env/bin/activate
-python3 TF_Amado.py --modeldir=Sample_TFLite_model
--- Object_Detection/mayrascript.sh
+++
@@ -1,3 +0,0 @@
-cd tflite1/
-source tflite1-env/bin/activate
-python3 TFLite_mayra.py --modeldir=Sample_TFLite_model
