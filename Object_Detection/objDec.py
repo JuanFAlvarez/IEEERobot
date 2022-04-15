396ea4b7335266efafe7d12466e758e187acc158
Henry Olvera <henry.olvera01@utrgv.edu>
2022-04-14 22:59:27 -0500

    Python example file

---
+++ Object_Detection/objDec.py
@@ -0,0 +1,28 @@
+# This file is the baseline we'll start with, we should be able to use this as a scaffolding 
+# for our program and modify it to our needs
+import jetson.inference
+import jetson.utils
+
+import argparse
+
+
+# parse the command line
+parser = argparse.ArgumentParser()
+parser.add_argument("filename", type=str, help="filename of the image to process")
+parser.add_argument("--network", type=str, default="googlenet", help="model to use, can be:  googlenet, resnet-18, ect.")
+opt = parser.parse_args()
+
+# load an image (into shared CPU/GPU memory)
+img = jetson.utils.loadImage(opt.filename)
+
+# load the recognition network
+net = jetson.inference.imageNet(opt.network)
+
+# classify the image
+class_idx, confidence = net.Classify(img)
+
+# find the object description
+class_desc = net.GetClassDesc(class_idx)
+
+# print out the result
+print("image is recognized as '{:s}' (class #{:d}) with {:f}% confidence".format(class_desc, class_idx, confidence * 100))
